8b51ef0b6682fff59311cfac30edc4c995e40317
Fábio Vilalba <fabiovilalba@Fabios-MacBook-Pro.local>
2023-06-08 19:18:24 -0300

    Create blog and read blog were created using gRPC

---
+++ blog/client/create.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	pb "github.com/fabiovalinhos/grpc-go/blog/proto"
+)
+
+func createBlog(c pb.BlogServiceClient) string {
+	log.Println("---createBlog was involked---")
+
+	blog := &pb.Blog{
+		AuthorId: "fabiovalinhos",
+		Title:    "My first blog",
+		Content:  "Content of first blog",
+	}
+
+	res, err := c.CreateBlog(context.Background(), blog)
+	if err != nil {
+		log.Fatalf("Unexpect error: %v\n", err)
+	}
+
+	log.Printf("Blog has been created: %s\n", res.Id)
+	return res.Id
+}
